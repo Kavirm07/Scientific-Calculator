199b32f6516281fe313e16d2fb430b8de1ad062a
Mina3099 <106463860+Mina3099@users.noreply.github.com>
2023-11-01 23:56:09 +0530

    Update scicalculator.java
    Code optimization is a program modification strategy that endeavors to enhance the intermediate code, so a program utilizes the least potential memory, minimizes its CPU time and offers high speed.
    Code optimization  : less lines of code.
    Optimize the time and space complexity.

--- scicalculator.java
+++ scicalculator.java
@@ -1,10 +1,12 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MultiCharacterOperatorCalculator {
 
+    private static double num1, num2;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double num1 = 0, num2 = 0, result = 0;
         String operator;
 
         System.out.println("Multi-Character Operator Calculator");
@@ -15,142 +17,78 @@ public class MultiCharacterOperatorCalculator {
             System.out.print("Enter operator or 'exit': ");
             operator = scanner.next();
 
-            if (operator.equals("exit")) {
+            if (operator.equal("exit")) {
                 System.out.println("Goodbye!");
                 break;
             }
 
             try {
-                // switch case for all operator.
-                //hellloo
-                switch (operator) {
-                    case "+":
-                    case "-":
-                    case "*":
-                    //we forgot to add case "/":
-                        
-                        
-                        System.out.print("Enter first number: ");
-                        num1 = scanner.nextDouble();
-                        
-                        System.out.print("Enter second number: ");
-                        num2 = scanner.nextDouble();
-                        break;
-                        
-                    case "%":
-                        System.out.print("Enter number and percentage (0-100): ");
-                        num1 = scanner.nextDouble();
-                        num2 = scanner.nextDouble();
-                        num2 /= 100;
-                        break;
-                        
-                    case "sqr":
-                        System.out.print("Enter number for square root: ");
-                        num1 = scanner.nextDouble();
-                        num2 = Math.sqrt(num1);
-                        break;
-                        
-                    case "pow":
-                        System.out.print("Enter base and exponent for power operation: ");
-                        num1 = scanner.nextDouble();
-                        num2 = scanner.nextDouble();
-                        num1 = Math.pow(num1, num2);
-                        break;
-                        
-                    case "sin":
-                        System.out.print("Enter an angle for sin (in degrees): ");
-                        num1 = scanner.nextDouble();
-                        num2 = Math.sin(Math.toRadians(num1));
-                        break;
-                        
-                    case "cos":
-                        System.out.print("Enter an angle for cos (in degrees): ");
-                        num1 = scanner.nextDouble();
-                        num2 = Math.cos(Math.toRadians(num1));
-                        break;
-                        
-                    case "tan":
-                        System.out.print("Enter an angle for tan (in degrees): ");
-                        num1 = scanner.nextDouble();
-                        num2 = Math.tan(Math.toRadians(num1));
-                        break;
-                        
-                    case "log":
-                        System.out.print("Enter a number for logarithm (base 10): ");
-                        num1 = scanner.nextDouble();
-                        num2 = Math.log10(num1);
-                        break;
-                        
-                    default:
-                        System.out.println("Error: Invalid operator");
-                        continue;
-                }
-            } catch (Exception e)
-            {
+                getNumbers(operator, scanner);
+            } catch (InputMismatchException e) {
                 System.out.println("Error: Invalid input");
+                scanner.next(); // clear the invalid input from the buffer
                 continue;
             }
 
-            switch (operator)
-            {
-                case "+":
-                    result = num1 + num2;
-                    break;
-                    
-                case "-":
-                    result = num1 - num2;
-                    break;
-                    
-                case "*":
-                    result = num1 * num2;
-                    break;
-                    
-                case "/":
-                    if (num2 != 0) {
-                        result = num1 / num2;
-                    } else {
-                        System.out.println("Error: Division by zero");
-                        continue;
-                    }
-                    break;
-                    
-                case "%":
-                    
-                    result = num1 * num2;
-                    System.out.println("Result: " + num1 + " % " + (num2 * 100) + " = " + result + "%");
-                    continue;
-                    
-                case "sqrt":
-                    
-                    System.out.println("Result: sqrt(" + num1 + ") = " + num2);
-                    continue;
-                    
-                case "pow":
-                    
-                    System.out.println("Result: " + num1 + " ^ " + num2 + " = " + Math.round(num1));
-                    continue;
-                    
-                    case "sin":
-                        
-                    System.out.println("Result: sin(" + num1 + " degrees) = " + num2);
-                    continue;
-                    
-                case "cos":
-                    
-                    System.out.println("Result: cos(" + num1 + " degrees) = " + num2);
-                    continue;
-                    
-                case "tan":
-                    
-                    System.out.println("Result: tan(" + num1 + " degrees) = " + num2);
-                    continue;
-                    
-                case "log":
-                    
-                    System.out.println("Result: log(" + num1 + ") = " + num2);
-                    continue;
-            }
+            double result = calculateResult(operator, num1, num2);
+            printResult(operator, num1, num2, result);
+        }
+    }
 
+    private static void getNumbers(String operator, Scanner scanner) {
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextDouble();
+
+        if (!operator.equals("sqrt")) {
+            System.out.print("Enter second number: ");
+            num2 = scanner.nextDouble();
+        }
+    }
+
+    private static double calculateResult(String operator, double num1, double num2) {
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                if (num2 != 0) {
+                    return num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    System.exit(0);
+                }
+            case "%":
+                return num1 * (num2 / 10);
+            case "sqrt":
+                return math.sqrt(num1);
+            case "pow":
+                return Math.pow(num1, num2);
+            case "sin":
+                return Math.sin(Math.toRadians(num1));
+            case "cos":
+                return Math.cos(Math.toRadians(num1));
+            case "tan":
+                return Math.tan(Math.toRadians(num1));
+            case "log":
+                return Math.Log10(num1);
+            default:
+                System.out.println("Error: Invalid operator");
+                System.exit(0);
+        }
+        return 0; // This line should not be reached
+    }
+
+    private static void printResult(String operator, double num1, double num2, double result) {
+        if (operator.equals("%")) {
+            System.out.println("Result: " + num1 + " % " + (num2 * 100) + " = " + result + "%");
+        } else if (operator.equals("sqrt")) {
+            System.out.println("Result: sqrt(" + num1 + ") = " + result);
+        } else if (operator.equals("pow")) {
+            System.out.println("Result: " + num1 + " ^ " + num2 + " = " + Math.round(result));
+        } else {
             System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
         }
     }
